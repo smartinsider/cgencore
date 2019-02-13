de299340a33a6078606efaa91642b9e201806d95
coin-universe <coin-universe@users.noreply.github.com>
2019-02-13 14:30:14 +0200

    Mining requer peers fixed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -220,7 +220,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = false; 
+        fMiningRequiresPeers = true; 
         fAllowMinDifficultyBlocks = false;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
